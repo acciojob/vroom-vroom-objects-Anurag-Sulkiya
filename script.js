19938c0d59cd89df564828fba4459811a963f1d1
Anurag Sulkiya <163811905+Anurag-Sulkiya@users.noreply.github.com>
2025-10-25 16:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	constructor(make, model){
+	 this._make = make;
+	 this._model = model;
+    }
 
-function SportsCar(make, model, topSpeed) {}
+	getMakeModel(){
+      return this._make + this._model;
+    }
+
+function SportsCar(make, model, topSpeed) extends Car(make, model){
+	constructor(make, model, topSpeed)
+	{
+        super(make, model);
+		this._topSpeed = topSpeed;
+	}
+
+	getTopSpeed(){
+    return this._topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
