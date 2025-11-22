17e0f6fc7cea6f2948530d7f6bac89d221c6e5cc
Anurag Sulkiya <163811905+Anurag-Sulkiya@users.noreply.github.com>
2025-11-23 03:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ Car.prototype.getMakeModel = function(){
     return this._make +" "+this._model;
 }
 
-function SportsCar(make, modal, topSpeed){
+function SportsCar(make, model, topSpeed){
 Car.call(this, make, model);
 this._topSpeed = topSpeed;
 }
@@ -19,7 +19,7 @@ SportsCar.prototype.getTopSpeed = function(){
 	return this._topSpeed;
 };
 
-const Car1 = new SportsCar("Ferrari", "Testarossa", 200);
+const car = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel()); 
-console.log(car.getTopSpeed()); 
+console.log(Car.getTopSpeed()); 
 
