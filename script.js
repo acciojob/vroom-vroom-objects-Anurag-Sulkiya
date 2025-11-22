6f14339a7d10a12cc55cd1be40d18996c7c13493
Anurag Sulkiya <163811905+Anurag-Sulkiya@users.noreply.github.com>
2025-11-23 03:28:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ SportsCar.prototype.getTopSpeed = function(){
 	return this._topSpeed;
 };
 
-const Car = new SportsCar("Ferrari", "Testarossa", 200);
+const Car1 = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel()); 
 console.log(car.getTopSpeed()); 
 
